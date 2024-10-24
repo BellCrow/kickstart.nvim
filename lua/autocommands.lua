ea8c88d45d4076d230e87aa94a20b59d686ea23a
Joshua Krieger <peremptor1@gmx.de>
2024-10-24 22:11:03 +0200

    - add autocommand to change neo tree to the CWD, that was given via args - KNOWN ISSUE: if you use nvim to edit commit messages he tries to   change in to the path of the msg as if it were a file

--- lua/autocommands.lua
+++ lua/autocommands.lua
@@ -11,3 +11,27 @@ vim.api.nvim_create_autocmd('TextYankPost', {
     vim.highlight.on_yank()
   end,
 })
+
+-- if vim was called with an argument specifying a directory, make that directory the CWD
+-- taken from: https://www.reddit.com/r/neovim/comments/1du176f/how_to_align_cwd_with_opened_folder/
+vim.api.nvim_create_autocmd('VimEnter', {
+  pattern = '*',
+  callback = function()
+    local from = vim.uv.cwd()
+    local target
+    local args = vim.fn.argv()
+
+    for _, arg in ipairs(type(args) == 'table' and args or {}) do
+      if vim.fn.isdirectory(arg) then
+        target = vim.fn.fnamemodify(arg, ':p')
+        print(string.format('Changing CWD from %s to %s', from, arg))
+
+        if target ~= nil then
+          vim.cmd(string.format(':cd %s', target))
+          vim.notify(string.format('cd to %s', target))
+        end
+      end
+    end
+    return true
+  end,
+})
